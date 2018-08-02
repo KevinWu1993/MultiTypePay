7589b53617854309c98c90659cd1bfdd7a2b750b
KevinWu <wqd1993@qq.com>
2018-08-02 09:58:41 +0800

    Fix AliPayType sandbox's bug

--- lib-pay-alipay/src/main/java/cn/kevinwu/lib_pay_alipay/AliPayType.java
+++ lib-pay-alipay/src/main/java/cn/kevinwu/lib_pay_alipay/AliPayType.java
@@ -3,6 +3,7 @@ package cn.kevinwu.lib_pay_alipay;
 import android.content.Intent;
 
 import com.alipay.sdk.app.EnvUtils;
+
 import cn.kevinwu.lib_pay_core.core.MultiPayContext;
 import cn.kevinwu.lib_pay_core.paytype.AbsPayType;
 
@@ -10,13 +11,14 @@ public class AliPayType extends AbsPayType<AliPayType> {
 
     @Override
     protected void startTypePay() {
-        if(isSandbox) EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX);
-        if( paramStr!=null ){
+        if (isSandbox) EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX);
+        else EnvUtils.setEnv(EnvUtils.EnvEnum.ONLINE);
+        if (paramStr != null) {
             Intent intent = new Intent(MultiPayContext.getInstance().getContext(), AliPayLoadActivity.class);
             intent.putExtra("paramString", paramStr);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             MultiPayContext.getInstance().getContext().startActivity(intent);
-        }else{
+        } else {
             MultiPayContext.getInstance().getPayResultCallBack().onError(AliPayStatusCode.ALI_PAY_PARAM_INVALID);
         }
     }
